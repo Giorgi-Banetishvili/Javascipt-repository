a8787b8f0cd6a604e415befec975f732f8e05713
Giorgi-Banetishvili <georgebanetishvili@gmail.com>
2024-08-18 10:37:44 +0400

    Add files via upload

---
+++ JS Dice roller/main.js
@@ -0,0 +1,19 @@
+function rollDice(){
+
+    const numberOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("result");
+    const diceImages = document.getElementById("images");
+    const values = [];
+    const images = [];
+
+
+    for(let i = 0; i < numberOfDice; i++ ){
+        const value = Math.floor(Math.random() * 6) + 1;
+       values.push(value);
+       images.push(`<img src="diceImagees/${value}.png">`);
+    }
+    
+    diceResult.textContent = `dice ${values.join(', ')}`
+    diceImages.innerHTML = images.join(' ');
+}
+
